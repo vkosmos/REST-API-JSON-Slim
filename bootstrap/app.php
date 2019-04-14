c1b3395b79469015002e6e7dda9fec9c61dbaf0e
vkosmos <korolev.dm.va@gmail.com>
2019-04-14 17:27:37 +0300

    REST API реализовано.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,18 +10,5 @@ $app = new App([
 
 $container = $app->getContainer();
 
-$container['view'] = function ($container) {
-    $view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
-        'cache' => false,
-    ]);
-
-    // Instantiate and add Slim specific extension
-    $router = $container->get('router');
-    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
-    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
-    return $view;
-};
-
-
 require __DIR__ . '/../routes/web.php';
 require __DIR__ . '/../routes/api.php';
